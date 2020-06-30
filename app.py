c47d82499e640137936dc1d7021e8530851ed9b8
Arun Pandian R <smartboyarun56@gmail.com>
2020-06-30 07:49:46 +0530

    Added Sample Image Links

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def upload_file():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(threaded=True, port=5000)
+	# app.run(threaded=True, port=5000)
+	app.run(debug=True)
